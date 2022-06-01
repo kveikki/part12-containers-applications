ecd3880ea529ca3ff51f4a97127dd5970f7c1225
kveikki <vertti.viitanen@wunderdog.fi>
2022-06-01 10:48:56 +0300

    Exercises 12.13-12.15

---
+++ todo-app/todo-frontend/src/Todos/Todo.js
@@ -0,0 +1,34 @@
+const Todo = ({ todo, deleteTodo, completeTodo }) => {
+
+    const doneInfo = (
+        <>
+          <span>This todo is done</span>
+          <span>
+            <button onClick={deleteTodo(todo)}> Delete </button>
+          </span>
+        </>
+      )
+
+      const notDoneInfo = (
+        <>
+          <span>
+            This todo is not done
+          </span>
+          <span>
+            <button onClick={deleteTodo(todo)}> Delete </button>
+            <button onClick={completeTodo(todo)}> Set as done </button>
+          </span>
+        </>
+      )
+
+      return (
+        <div key={todo.text} style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
+          <span>
+            {todo.text} 
+          </span>
+          {todo.done ? doneInfo : notDoneInfo}
+        </div>
+      )
+}
+
+export default Todo;
